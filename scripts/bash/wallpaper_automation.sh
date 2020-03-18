bfdd0299cde716393f6b44bec58801a787911881
channel-42 <hallowmd@gmail.com>
2020-03-18 21:26:11 +0100

    Adding user specified wallpaper setting

--- scripts/bash/wallpaper_automation.sh
+++ scripts/bash/wallpaper_automation.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 #Channel_42
 #This script selects and applies a random wallaper from the ~/Pictures/Wallpapers Directory
+#Or it searches for a user-defined file
 
 DIR="$HOME/Pictures/Wallpapers/"   #Wallpaper Directory
-cd "$DIR"               #change to wpp dir
-WALLS=( $(ls) )         #create array with all wallpapers
-RANDOM=$$$(date +%s)    #generate random number
 
-selectedwall=${WALLS[$RANDOM % ${#WALLS[@]} ]}  #select a random wallpaper
+case "$1" in                    #check if user has inputed request
+    "")                         #if no imput, randowm wallpaper
+        cd "$DIR"               #change to wpp dir
+        WALLS=( $(ls) )         #create array with all wallpapers
+        RANDOM=$$$(date +%s)    #generate random number
+        selectedwall=${WALLS[$RANDOM % ${#WALLS[@]} ]}  #select a random wallpaper
+        final="${DIR}${selectedwall}"   #join strings for final command
+        ;;
+    *)                          #if input, user wallpaper
+        if test -f "$DIR""$1"; then #check files existance
+            final="$DIR""$1"
+        else
+            echo "File not found"   #on error
+        fi
+esac
 
-final="${DIR}${selectedwall}"   #join strings for final command
-
-feh --bg-fill "$final"         #set wallpaper
+feh --bg-fill "$final" &>/dev/null    #set wallpaper; supress errors
