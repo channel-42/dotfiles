e1273b5cbf067168f8844082d41afe9a1a01aa15
channel-42 <hallowmd@gmail.com>
2020-03-26 23:45:07 +0100

    Adding 'wait on error' for python files

--- scripts/bash/compiler.sh
+++ scripts/bash/compiler.sh
@@ -8,11 +8,11 @@ base="${file%.*}"
 
 #goto files dir
 cd "$dir" || exit
-
+clear
 #do the followeing depending on file's suffix
 case "$file" in
     *\.md) pandoc "$file" --pdf-engine=xelatex -V fontsize=10pt -V mainfont="Noto Sans" -V monofont="Source Code Pro" -o "$base".pdf && [[ 'echo $?' ]] || read -p "Press enter to continue";;
-    *\.py) python3 "$file" && sleep 3;;
+    *\.py) python3 "$file" && [[ 'echo $?' ]] && sleep 2 || read -p "Press enter to continue" ;;
     *\.c) gcc "$file" -Wall -o "$base".out && [[ 'echo $?' ]] && "$base".out && sleep 2 || read -p "Press enter to continue" ;; #if gcc returns 0 then exec, else show compile error and await user input
     *\.sh) bash "$file" && [[ 'echo $?' ]] || read -p "Press enter to continue" ;;
     *\.tex) xelatex "$file" && [[ 'echo $?' ]] || read -p "Press enter to continue";; 
