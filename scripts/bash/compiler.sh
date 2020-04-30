19eb98189e60f430332f24cc102db4cf161a916d
channel-42 <59137829+channel-42@users.noreply.github.com>
2020-04-30 21:13:47 +0200

    Adding zathurarc and minor tweaks

--- scripts/bash/compiler.sh
+++ scripts/bash/compiler.sh
@@ -11,7 +11,8 @@ cd "$dir" || exit
 clear
 #do the followeing depending on file's suffix
 case "$file" in
-    *\.md) pandoc "$file" --pdf-engine=xelatex -V fontsize=10pt -V mainfont="Noto Sans" -V monofont="Source Code Pro" -o "$base".pdf --highlight-style=breezedark -H $HOME/Documents/markdown/code-block.tex && [[ 'echo $?' ]] || read -p "Press enter to continue";;
+    *\.md) pandoc "$file" --pdf-engine=xelatex -V mainfont="Noto Sans" -V monofont="Source Code Pro" -o "$base".pdf --highlight-style=breezedark  && [[ 'echo $?' ]] || read -p "Press enter to continue";;
+    #*\.md) pandoc "$file" --pdf-engine=xelatex -V fontsize=10pt -V mainfont="Noto Sans" -V monofont="Source Code Pro" -o "$base".pdf --highlight-style=breezedark -H $HOME/Documents/markdown/code-block.tex && [[ 'echo $?' ]] || read -p "Press enter to continue";;
     *\.py) python3 "$file" && [[ 'echo $?' ]] && sleep 2 || read -p "Press enter to continue" ;;
     *\.c) gcc "$file" -Wall -o "$base".out -lm && [[ 'echo $?' ]] && "$base".out && sleep 2 || read -p "Press enter to continue" ;; #if gcc returns 0 then exec, else show compile error and await user input
     *\.sh) bash "$file" && [[ 'echo $?' ]] && sleep 2 || read -p "Press enter to continue" ;;
