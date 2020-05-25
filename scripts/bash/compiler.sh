2d38a94145e87b045189f96e8fac250bdc8fc756
Channel_42 <59137829+channel-42@users.noreply.github.com>
2020-05-25 18:04:13 +0200

    Minor tweaks

--- scripts/bash/compiler.sh
+++ scripts/bash/compiler.sh
@@ -13,7 +13,7 @@ cd "$dir" || exit
 case "$file" in
     *\.md) pandoc "$file" --pdf-engine=xelatex -V fontsize=10pt -V mainfont="Noto Sans" -V monofont="Source Code Pro" -o "$base".pdf && [[ 'echo $?' ]] || read -p "Press enter to continue";;
     *\.py) python3 "$file" && sleep 3;;
-    *\.c) gcc "$file" -Wall -o "$base".out && [[ 'echo $?' ]] && "$base".out && sleep 2 || read -p "Press enter to continue" ;; #if gcc returns 0 then exec, else show compile error and await user input
+    *\.c) gcc "$file" -Wall -o "$base".out -lm && [[ 'echo $?' ]] && "$base".out && sleep 2 || read -p "Press enter to continue" ;; #if gcc returns 0 then exec, else show compile error and await user input
     *\.sh) bash "$file" && [[ 'echo $?' ]] && sleep 2 || read -p "Press enter to continue" ;;
     *\.tex) xelatex "$file" && [[ 'echo $?' ]] || read -p "Press enter to continue";; 
     *) 
