6640a5ff55a5f7be014a2526b7ed36e8d07c264b
channel-42 <59137829+channel-42@users.noreply.github.com>
2020-12-07 17:02:28 +0100

    Minor Changes

--- scripts/bash/emoji.sh
+++ scripts/bash/emoji.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #show rofi list of emojis and copy output to clipboard
 
-cat $HOME/.local/share/emoji | cut -d";" -f1 | rofi -dmenu -lines -3 -eh 2 -width 20 -font "Apple Color Emoji 15" -theme /usr/share/rofi/themes/sidebar.rasi | awk '{print $1}' | tr -d '\n' | xclip -selection clipboard
+cat $HOME/.local/share/emoji | cut -d";" -f1 | rofi -dmenu -eh 1 -font "Apple Color Emoji 15" -theme sidebar | awk '{print $1}' | tr -d '\n' | xclip -selection clipboard
