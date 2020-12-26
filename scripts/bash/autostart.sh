fcaabf90a5227d80e7695afecbce8febf1c3b93d
channel-42 <59137829+channel-42@users.noreply.github.com>
2020-12-26 21:54:42 +0100

    minor changes

--- scripts/bash/autostart.sh
+++ scripts/bash/autostart.sh
@@ -3,6 +3,7 @@
 # Terminate already running bar instances
 killall -q polybar
 
+cool=$(liquidctl set sync color fading 002b36 268bd2 2aa198 657b83 6c71c4 859900 93a1a1 b58900)
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
